6a97aac0614986b52c0685bfb47c96d35e230b7b
Federico Tensi <federicotensi@gmail.com>
2023-12-02 17:56:19 +0100

    Create Day1.ts

---
+++ Day1.ts
@@ -0,0 +1,36 @@
+const longText = `
+  threehqv2
+  sxoneightoneckk9ldctxxnffqnzmjqvj
+  ...All the strings
+  nine1ztqbs
+  eightndxxqxtwo3cqz47
+  fiveeight792eightqskstrftdpccsrgskrhc
+  26fmrrhhpthree6b`
+const inputs = longText.trim().split('\n');
+
+
+
+const isNumber = (n: string) => {
+  return !isNaN(parseFloat(n)) && isFinite(parseFloat(n));
+};
+
+const allNumbers = [];
+for (let j = 0; j < inputs.length; j++) {
+  const numberArray = [];
+  const string = inputs[j];
+  for (let i = 0; i < string.length; i++) {
+    const word = string[i];
+
+    if (isNumber(word)) {
+      numberArray.push(Number(word));
+    }
+  }
+ 
+  const concatenatedNumber = Number(`${numberArray[0]}${numberArray[numberArray.length -1]}`)
+  allNumbers.push(concatenatedNumber)
+
+}
+
+console.log(allNumbers)
+const sum = allNumbers.reduce((a, b) => a + b, 0);
+console.log(sum);
